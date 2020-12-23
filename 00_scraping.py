c9a88500c38f17c41f0515d28c5f602dc503cd48
Edoardo <gatto@nanowar.it>
2020-12-23 14:17:18 +0100

    spider

---
+++ 00_scraping.py
@@ -0,0 +1,18 @@
+import scrapy
+
+
+class FirstSpider(scrapy.Spider):
+
+    spider_name = 'Books'
+    start_url = [
+            'https://books.toscrape.com/',
+            'https://books.toscrape.com/catalogue/immunity-how-elie-metchnikoff-changed-the-course-of-modern-medicine_900/index.html'
+            ]
+        
+
+    def parse(self, response):
+        page = response.url.split('/')[-2]
+        filename = 'books-%s.html' % page
+
+        with open(filenanme, 'wb') as f:
+            f.write(response.body)
