df8f2e9f6e2a92a7a50ba6406c0a9bc12985e689
Edoardo <gatto@nanowar.it>
2021-03-31 10:22:59 +0200

    last updates

--- MTG_Scraper/create_windows_exe.sh
+++ MTG_Scraper/create_windows_exe.sh
@@ -5,9 +5,11 @@ mtg_windows='/home/edoardo/Dropbox/MTG_Scraper/'
 cp *.txt $mtg_windows
 cp *.csv $mtg_windows
 
-pyinstaller --onefile mtg_scraper.py
+#pyinstaller --onefile mtg_scraper.py
+pyinstaller='/home/edoardo/.wine/drive_c/users/edoardo/Local\ Settings/Application Data/Programs/Python/Python36-32/Scripts/pyinstaller.exe'
+wine $pyinstaller --onefile mtg_scraper.py
 
-cp dist/mtg_scraper $mtg_windows/'mtg_scraper.exe'
+#cp dist/mtg_scraper $mtg_windows/'mtg_scraper.exe'
 
 
 
